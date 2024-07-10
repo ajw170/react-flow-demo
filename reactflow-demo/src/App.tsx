38a85a3ee2a311ae3b475cd3987559fbf0fc9f9f
Andrew Wood <andrew.wood@contractor.swri.org>
2024-07-09 23:52:08 -0400

    first draft

--- reactflow-demo/src/App.tsx
+++ reactflow-demo/src/App.tsx
@@ -1,38 +1,94 @@
-import { useState } from 'react'
+import {useState, useCallback, useMemo} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {
+    ReactFlow,
+    MiniMap,
+    Controls,
+    Background,
+    useNodesState,
+    useEdgesState,
+    addEdge,
+    type Node
+} from "@xyflow/react";
+import '@xyflow/react/dist/style.css';
+import {AndNode} from "./nodes/andNode";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-        <div className={"container"}>
-            <div className="left-div">
-                <h1>Vite + React</h1>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
+    const initialNodes = [
+        {id: '1', position: {x: 0, y: 0}, type: 'and', data: {color: 'green'}},
+        {id: '2', position: {x: 0, y: 100}, type: 'and', data: {color: 'red'}},
+    ];
+    const initialEdges = [{id: 'e1-2', source: '1', target: '2'}];
+
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+    const nodeTypes = useMemo(() => ({and: AndNode}), []);
+
+    const onConnect = useCallback(
+        (params) => setEdges((eds) => addEdge(params, eds)),
+        [setEdges],
+    );
+
+    return (
+        <>
+            <div className={"container"}>
+                <div className="left-div">
+                    <div style={{width: '100%', height: '100%'}}>
+                        <ReactFlow nodes={nodes} nodeTypes={nodeTypes} edges={edges} onNodesChange={onNodesChange}
+                                   onEdgesChange={onEdgesChange} onConnect={onConnect}>
+                            <Controls/>
+                            <Background variant="dots" gap={12} size={1}/>
+                        </ReactFlow>
+                    </div>
+                </div>
+                <div className="right-div">
+                    <div><h3>Combinators</h3></div>
+                    <div>
+                        {
+                            ['and', 'or', 'not'].map((type) => (
+                                <div key={type}
+                                     onDragStart={(event) => event.dataTransfer.setData('application/reactflow', type)}
+                                     draggable
+                                     style={{
+                                         padding: '10px',
+                                         margin: '10px',
+                                         border: '1px solid gray',
+                                         borderRadius: '5px',
+                                         backgroundColor: type === 'and' ? 'lightgreen' : type === 'or' ? 'lightyellow' : type === 'not' ? 'lightpink' : 'lightblue'
+                                     }}>
+                                    {type}
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <div><h3>Specifiers</h3></div>
+                    <div>
+                        {
+                            ['Velocity', 'Occupancy', 'Volume'].map((type) => (
+                                <div key={type}
+                                     onDragStart={(event) => event.dataTransfer.setData('application/reactflow', type)}
+                                     draggable
+                                     style={{
+                                         padding: '10px',
+                                         margin: '10px',
+                                         border: '1px solid gray',
+                                         borderRadius: '5px',
+                                         backgroundColor: type === 'and' ? 'lightgreen' : type === 'or' ? 'lightyellow' : type === 'not' ? 'lightpink' : 'lightblue'
+                                     }}>
+                                    {type}
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
             </div>
-            <div className="right-div">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-                <p className="read-the-docs">
-                    Click on the Vite and React logos to learn more
-                </p>
-            </div>
-        </div>
-      
-    </>
-  )
+        </>
+    )
 }
 
 export default App
---
+++ reactflow-demo/src/nodes/andNode.tsx
@@ -0,0 +1,26 @@
+﻿import {useCallback} from 'react';
+import {Handle, Position} from '@xyflow/react';
+import type {Node, NodeProps} from '@xyflow/react';
+
+type AndNode = Node<{}, 'and'>
+
+/**
+ * The and node.
+ * @param data THe data for the node, if applicable.
+ */
+export function AndNode({data}): NodeProps<AndNode> {
+    return (
+        <>
+            <Handle type="target" position={Position.Top}/>
+            <div>
+                <h3>And</h3>
+            </div>
+            <Handle type="source" position={Position.Bottom} id="a"/>
+            <Handle
+                type="source"
+                position={Position.Bottom}
+                id="b"
+            />
+        </>
+    );
+}
